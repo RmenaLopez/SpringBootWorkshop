d656105f4fae373565bc3ca3f9b4b4162a4df1cc
Rafael Mena <rmena@NSL-RMENAs-MacBook-Pro.local>
2018-08-31 13:08:42 -0700

    first commit

---
+++ springboot-workshop/src/main/java/com/ns/springboot/workshop/springbootworkshop/SpringbootWorkshopApplication.java
@@ -0,0 +1,12 @@
+package com.ns.springboot.workshop.springbootworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWorkshopApplication.class, args);
+	}
+}
